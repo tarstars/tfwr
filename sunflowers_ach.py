00b4c054dc8205ac6a7ae8b085add289cdbd161d
tarstars <142857@mai.ru>
2025-12-10 21:54:28 +0300

    map reduce

--- sunflowers_ach.py
+++ sunflowers_ach.py
@@ -1,7 +1,5 @@
 def go_best(ld, rd, ln, rn):
-	d, n = ld, ln
-	if rn < ln:
-		d, n = rd, rn
+	d, n = [(ld, ln), (rd, rn)][rn < ln]
 	for _ in range(n):
 		move(d)
 
@@ -9,39 +7,65 @@ def go_best(ld, rd, ln, rn):
 def nav(x2, y2):
 	n = get_world_size()
 	x1, y1 = get_pos_x(), get_pos_y()
-	go_best(West, East, (x1 - x2) % n, (x2 - x1)%n)
-	go_best(South, North, (y1 - y2) % n, (y2 - y1)%n)
+	go_best(West, East, (x1 - x2) % n, (x2 - x1) % n)
+	go_best(South, North, (y1 - y2) % n, (y2 - y1) % n)
 
 
 def till_soil():
 	if get_ground_type() != Grounds.Soil:
 		till()
 		  
-
-def each_column(f):
+def drone_map_reduce(objects, f_map, f_red):
 	drone_jobs = []
 
 	for _ in range(max_drones()):
 		drone_jobs.append([])
 	
-	for q in range(get_world_size()):
+	for q in objects:
 		drone_jobs[q % len(drone_jobs)].append(q)
 
-	def drone_job(columns):
+	results = []
+	drones = []
+
+	while drone_jobs:
+		while drone_jobs and num_drones() < max_drones():
+			current = drone_jobs.pop()
+			drones.append(spawn_drone(f_map(current)))
+		if drone_jobs:
+			current = drone_jobs.pop()
+			results.append(f_map(current)())
+
+	for current_drone in drones:
+		results.append(wait_for(current_drone))
+
+	reduced = None
+
+	for r in results:
+		if reduced == None:
+			reduced = r
+		else:
+			reduced = f_red(reduced, r)
+	
+	return reduced
+
+def no_reduce(a, b):
+	pass
+
+def till_field():
+	columns = []
+
+	for x in range(get_world_size()):
+		columns.append(x)
+
+	def till_column(columns):
 		def g():
 			for q in columns:
 				for p in range(get_world_size()):
 					nav(q, p)
-					f()
+					till_soil()
 		return g
-	
-	while drone_jobs:
-		while drone_jobs and num_drones() < max_drones():
-			current = drone_jobs.pop()
-			spawn_drone(drone_job(current))
-		if drone_jobs:
-			current = drone_jobs.pop()
-			drone_job(current)()
+
+	drone_map_reduce(columns, till_column, no_reduce)
 
  
 def plant_sunflowers():
@@ -110,7 +134,8 @@ def gather_all(cells):
 			current = drone_jobs.pop()
 			drone_job(current)()
 
-each_column(till_soil)
+
+till_field()
 while True:
 	height_map = plant_sunflowers()
 	height2cell = dict()
